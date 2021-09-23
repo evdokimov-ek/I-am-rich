8acb01a264327e9df5a66c46f9b60f619f9eb02a
Evgeniy Evdokimov <evdokimov.ek@gmail.com>
2021-09-23 20:24:45 +0300

    Initial Commit

---
+++ I am rich/I_am_richApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_richApp.swift
+//  I am rich
+//
+//  Created by 19336088 on 23.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct I_am_richApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
